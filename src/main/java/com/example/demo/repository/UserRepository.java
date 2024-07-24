d57ac1b1f4e282c276e541ab95341d0bf22ef897
Zhmikhail <zhmikhail@MacBook-Pro-Zhmikhail.local>
2024-07-24 13:57:15 +0300

    update_new

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -33,7 +33,7 @@ public class UserRepository {
 
     public void update(UserEntity user) {
         String sqlQuery = QueryType.UPDATE_USER.getQuery();
-        jdbcTemplate.update(sqlQuery, user.getFirstName(), user.getLastName(), user.getBirthDate(), user.getPositionId(), user.getSalary(), user.getHireDate(), user.getFireDate());
+        jdbcTemplate.update(sqlQuery, user.getFirstName(), user.getLastName(), user.getBirthDate(), user.getPositionId(), user.getSalary(), user.getHireDate(), user.getFireDate(), user.getId());
     }
 
     public void delete(int id) {
@@ -42,7 +42,11 @@ public class UserRepository {
     }
 
     public enum QueryType {
-        SELECT_ALL_USERS(String.format("SELECT * FROM Users u JOIN position p ON p.%s = u.position_id", UserRowMapper.TableFields.ID.getTableField())), SELECT_USER(String.format("SELECT * FROM Users u JOIN position p on p.%s = u.position_id WHERE u.%s = ?", UserRowMapper.TableFields.ID.getTableField(), UserRowMapper.TableFields.ID.getTableField())), SAVE_USER(String.format("INSERT INTO Users (%s, %s, %s, %s, %s, %s, %s) VALUES (?, ?, ?, ?, ?, ?, ?)", UserRowMapper.TableFields.FIRST_NAME.getTableField(), UserRowMapper.TableFields.LAST_NAME.getTableField(), UserRowMapper.TableFields.BIRTH_DATE.getTableField(), UserRowMapper.TableFields.POSITION_ID.getTableField(), UserRowMapper.TableFields.SALARY.getTableField(), UserRowMapper.TableFields.HIRE_DATE.getTableField(), UserRowMapper.TableFields.FIRE_DATE.getTableField())), UPDATE_USER(String.format("UPDATE Users SET %s = ?, %s = ?, %s = ?, %s = ?, %s = ?, %s = ?, %s = ? WHERE %s = ?", UserRowMapper.TableFields.FIRST_NAME.getTableField(), UserRowMapper.TableFields.LAST_NAME.getTableField(), UserRowMapper.TableFields.BIRTH_DATE.getTableField(), UserRowMapper.TableFields.POSITION_ID.getTableField(), UserRowMapper.TableFields.SALARY.getTableField(), UserRowMapper.TableFields.HIRE_DATE.getTableField(), UserRowMapper.TableFields.FIRE_DATE.getTableField(), UserRowMapper.TableFields.ID.getTableField())), DELETE_USER(String.format("DELETE FROM Users WHERE %s = ?", UserRowMapper.TableFields.ID.getTableField()));
+        SELECT_ALL_USERS(String.format("SELECT * FROM Users u JOIN position p ON p.%s = u.position_id", UserRowMapper.TableFields.ID.getTableField())),
+        SELECT_USER(String.format("SELECT * FROM Users u JOIN position p on p.%s = u.position_id WHERE u.%s = ?", UserRowMapper.TableFields.ID.getTableField(), UserRowMapper.TableFields.ID.getTableField())),
+        SAVE_USER(String.format("INSERT INTO Users (%s, %s, %s, %s, %s, %s, %s) VALUES (?, ?, ?, ?, ?, ?, ?)", UserRowMapper.TableFields.FIRST_NAME.getTableField(), UserRowMapper.TableFields.LAST_NAME.getTableField(), UserRowMapper.TableFields.BIRTH_DATE.getTableField(), UserRowMapper.TableFields.POSITION_ID.getTableField(), UserRowMapper.TableFields.SALARY.getTableField(), UserRowMapper.TableFields.HIRE_DATE.getTableField(), UserRowMapper.TableFields.FIRE_DATE.getTableField())),
+        UPDATE_USER(String.format("UPDATE Users SET %s = ?, %s = ?, %s = ?, %s = ?, %s = ?, %s = ?, %s = ? WHERE %s = ?", UserRowMapper.TableFields.FIRST_NAME.getTableField(), UserRowMapper.TableFields.LAST_NAME.getTableField(), UserRowMapper.TableFields.BIRTH_DATE.getTableField(), UserRowMapper.TableFields.POSITION_ID.getTableField(), UserRowMapper.TableFields.SALARY.getTableField(), UserRowMapper.TableFields.HIRE_DATE.getTableField(), UserRowMapper.TableFields.FIRE_DATE.getTableField(), UserRowMapper.TableFields.ID.getTableField())),
+        DELETE_USER(String.format("DELETE FROM Users WHERE %s = ?", UserRowMapper.TableFields.ID.getTableField()));
 
         private final String query;
 
